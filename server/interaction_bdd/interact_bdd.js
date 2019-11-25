d45477e6abefac26ded5d388a93a61ee9d2b30ed
juquod <56772060+juquod@users.noreply.github.com>
2019-11-25 12:12:31 -0500

    Update interact_bdd.js

--- server/interaction_bdd/interact_bdd.js
+++ server/interaction_bdd/interact_bdd.js
@@ -480,7 +480,7 @@ function recup_trajet_reservations(id_user, callback=null){
 	}
 	CONFIG.connexion(NUM_FONC);
 
-	var requete=CONFIG.bdd.query('SELECT r1.`id_user`, r1.`nb_place`, r1.`etape_dep`, r1.`etape_arr`, t1.`date`, t1.`h_dep`, t1.`h_arr`, t1.`lieu_dep`, t1.`lieu_arr` FROM `reservation` r1 JOIN `trajet` t1 ON t1.`id_trajet`=r1.`id_trajet` WHERE r1.`id_user`='+id_user,
+	var requete=CONFIG.bdd.query('SELECT r1.`id_user`, r1.`nb_place`, r1.`etape_dep`, r1.`etape_arr`, t1.`date`, t1.`h_dep`, t1.`h_arr`, t1.`lieu_dep`, t1.`lieu_arr`, c1.`prenom`, c1.`nom` FROM `reservation` r1 JOIN `trajet` t1 ON t1.`id_trajet`=r1.`id_trajet` JOIN `user` c1 ON c1.`id_user`=t1.`id_conducteur` WHERE r1.`id_user`='+id_user,
 		function (err, result) {
 		    if(!err){
 			    if(callback!=null)callback(result);
